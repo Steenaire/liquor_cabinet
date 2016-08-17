c976f58458304f383558cb3bdf7c9a9235328abe
Steen Clarke <cmclarkey@gmail.com>
2016-08-17 11:14:48 -0700

    Move logic to model methods instead of controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,31 +3,16 @@ class RecipesController < ApplicationController
   def index
     @recipes = Recipe.all
 
-    if params[:personal_index]
+    if params[:personal_index_ignore_brand]
       users_ingredients = current_user.ingredients
       recipes_all = Recipe.all
-      @recipes = []
-      recipes_all.each do |recipe|
-        comparisons = recipe.ingredients - users_ingredients
-        @recipes << recipe if (comparisons-users_ingredients).empty?
-      end
+      @recipes = Recipe.check_user_inventory(recipes_all,users_ingredients)
     end
 
     if params[:personal_index_ignore_garnish]
       users_ingredients = current_user.ingredients
       recipes_all = Recipe.all
-      recipes_garnish_stripped = []
-      @recipes = []
-
-      recipes_all.each do |recipe|
-        recipe.recipe_ingredients.each do |recipe_ingredient|
-          recipes_garnish_stripped = recipe.ingredients - [recipe_ingredient.ingredient] if recipe_ingredient.garnish
-        end
-
-        comparisons = recipes_garnish_stripped - users_ingredients
-        @recipes << recipe if (comparisons-users_ingredients).empty?
-      end
-
+      @recipes = Recipe.strip_garnishes(recipes_all,users_ingredients)
     end
 
   end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -9,4 +9,32 @@ class Recipe < ApplicationRecord
     where("lower(name) LIKE ?", "%#{search.downcase}%") 
   end
 
+  def self.check_user_inventory(recipes_all,users_ingredients)
+    recipes = []
+    recipes_all.each do |recipe|
+      comparisons = recipe.ingredients - users_ingredients
+      recipes << recipe if (comparisons-users_ingredients).empty?
+    end
+    return recipes
+  end
+
+  def self.strip_garnishes(recipes_all,users_ingredients)
+      recipes_garnish_stripped = []
+
+      recipes_all.each do |recipe|
+        recipe.recipe_ingredients.each do |recipe_ingredient|
+          recipes_garnish_stripped = recipe.ingredients - [recipe_ingredient.ingredient] if recipe_ingredient.garnish
+        end
+      end
+
+      recipes = []
+
+      recipes_all.each do |recipe|
+        comparisons = recipes_garnish_stripped - users_ingredients
+        recipes << recipe if (comparisons-users_ingredients).empty?
+      end
+
+      return recipes
+  end
+
 end
