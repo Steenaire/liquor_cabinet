3b6f2b64709bcbe6d3f96e7af3add906b725cf08
Steen Clarke <cmclarkey@gmail.com>
2016-08-17 12:08:07 -0700

    Put garnish stripping back into model for recipes, fixed logic

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -19,17 +19,15 @@ class Recipe < ApplicationRecord
   end
 
   def self.strip_garnishes(recipes_all,users_ingredients)
+
       recipes_garnish_stripped = []
+      recipes = []
 
       recipes_all.each do |recipe|
         recipe.recipe_ingredients.each do |recipe_ingredient|
           recipes_garnish_stripped = recipe.ingredients - [recipe_ingredient.ingredient] if recipe_ingredient.garnish
         end
-      end
 
-      recipes = []
-
-      recipes_all.each do |recipe|
         comparisons = recipes_garnish_stripped - users_ingredients
         recipes << recipe if (comparisons-users_ingredients).empty?
       end
