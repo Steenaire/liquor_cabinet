30598c2c4fddd1b54939d42e95328b3028110196
Steen Clarke <cmclarkey@gmail.com>
2016-08-17 12:09:18 -0700

    Refactor recipes controller index action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,12 @@
 class RecipesController < ApplicationController
 
   def index
+      users_ingredients = current_user.ingredients
+      recipes_all = Recipe.all
 
     if params[:personal_index_ignore_brand]
-      users_ingredients = current_user.ingredients
-      recipes_all = Recipe.all
       @recipes = Recipe.check_user_inventory(recipes_all,users_ingredients)
     elsif params[:personal_index_ignore_garnish]
-      users_ingredients = current_user.ingredients
-      recipes_all = Recipe.all
       @recipes = Recipe.strip_garnishes(recipes_all,users_ingredients)
     else
       @recipes = Recipe.all
