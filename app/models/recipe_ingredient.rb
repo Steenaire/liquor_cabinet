237efa7b04d46b00cdcea20a992e292d37c50edb
Steen Clarke <cmclarkey@gmail.com>
2016-08-16 18:06:12 -0700

    Fix issue with oz_to_ml so counts don't get converted

--- app/models/recipe_ingredient.rb
+++ app/models/recipe_ingredient.rb
@@ -4,12 +4,14 @@ class RecipeIngredient < ApplicationRecord
 
   def measure_unit
     return "#{self.quantity}oz" if self.ingredient.volume || self.ingredient.weight
-    return "#{self.quantity.round} dash" if self.ingredient.dash
+    return "#{self.quantity.round} dash" if self.ingredient.dash && self.quantity == 1
+    return "#{self.quantity.round} dashes" if self.ingredient.dash
     return "#{self.quantity.round} " if self.ingredient.count
   end
 
   def oz_to_ml
-    return "#{(self.quantity*29.57).round}mL"
+    return "#{(self.quantity*29.57).round}mL" if self.ingredient.volume
+    return "#{(self.quantity).round} "
   end
 
 end
