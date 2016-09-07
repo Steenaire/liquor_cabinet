cd40ccbff3d3639fd26286d058a517a018ea4c09
Steen Clarke <cmclarkey@gmail.com>
2016-09-06 18:28:40 -0700

    Add Geocoder, allow to store user location data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,13 +7,22 @@ class UsersController < ApplicationController
     user = User.new(
       username: params[:username].gsub(/\b('?[a-z])/) { $1.capitalize },
       email: params[:email].downcase,
+      address: params[:address],
+      city: params[:city],
+      zip_code: params[:zip_code],
       password: params[:password],
       password_confirmation: params[:password_confirmation]
       )
+    if params[:address] || params[:city] || params[:zip_code]
+      address = "#{params[:address]}, #{params[:city]} #{params[:zip_code]}"
+      city = Geocoder.search(address)
+      user.latitude = city[0].latitude
+      user.longitude = city[0].longitude
+    end
 
     if user.save
       session[:user_id] = user.id
-      flash[:success] = "Successfully created account! Welcome, #{params[:name]}!"
+      flash[:success] = "Successfully created account! Welcome, #{params[:username]}!"
       redirect_to "/"
     else
       flash[:danger] = "Invalid email or password."
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160904231417) do
+ActiveRecord::Schema.define(version: 20160907011129) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,11 @@ ActiveRecord::Schema.define(version: 20160904231417) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "address"
+    t.string   "city"
+    t.string   "zip_code"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
