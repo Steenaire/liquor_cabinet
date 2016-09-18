236bb2303eff62db531c95f238f1bde2dc8b697e
Steen Clarke <cmclarkey@gmail.com>
2016-09-18 13:53:32 -0700

    Add autocomplete for recipes search

--- app/assets/javascripts/recipes_ctrl.js
+++ app/assets/javascripts/recipes_ctrl.js
@@ -1,6 +1,13 @@
 (function() {
     'use strict';
     angular.module("app").controller("recipesCtrl", function($scope, $http) {
+        $scope.setup = function() {
+            $http.get("/api/v1/recipes.json").then(function(response) {
+                $scope.recipes = response.data;
+            }, function(error) {
+                console.log("sad path");
+            });
+        }
 
     });
 })();
