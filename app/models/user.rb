b13e4063f6b7cdd98bc41740c5db229797e61cce
Steen Clarke <cmclarkey@gmail.com>
2016-09-18 10:02:53 -0700

    Implement a user model method to show popular drinks by location, based on timeline drinks

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,25 @@ class User < ApplicationRecord
   has_many :timeline_drinks
   has_many :ratings
 
-  geocoded_by :full_street_address   # can also be an IP address
+  geocoded_by :address   # can also be an IP address
   after_validation :geocode          # auto-fetch coordinates
+
+  def my_area_drinks
+    nearby_drinks = {}
+    nearby_users = User.near("#{self.latitude}, #{self.longitude}")
+    nearby_users.each do |user|
+      if user.timeline_drinks.any?
+        user.timeline_drinks.each do |drink|
+          if nearby_drinks[drink]
+            nearby_drinks[drink.recipe] += 1
+          else
+            nearby_drinks[drink.recipe] = 1
+          end
+        end
+      end
+    end
+    
+    return nearby_drinks
+  end
   
 end
