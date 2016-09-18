f2392fb9b40383780bbea4b4ec4e2ce77ccb0b2f
Steen Clarke <cmclarkey@gmail.com>
2016-09-18 15:32:35 -0700

    Change recipes_ctrl.js so  won't get minified

--- app/assets/javascripts/recipes_ctrl.js
+++ app/assets/javascripts/recipes_ctrl.js
@@ -1,6 +1,6 @@
 (function() {
     'use strict';
-    angular.module("app").controller("recipesCtrl", function($scope, $http) {
+    angular.module("app").controller("recipesCtrl", ['$scope', function($scope, $http) {
         $scope.setup = function() {
             $http.get("/api/v1/recipes.json").then(function(response) {
                 $scope.recipes = response.data;
@@ -15,5 +15,5 @@
             });
         }
 
-    });
+    }]);
 })();
