68c5f58530ddf3c69e7d5bd4a1b65131d6a63b34
Steen Clarke <cmclarkey@gmail.com>
2016-09-20 20:40:10 -0700

    Make check boxes for recipe params permutations to improve UI

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,6 +6,7 @@ class IngredientsController < ApplicationController
   def create
     @ingredient = Ingredient.new(name: params[:name].gsub(/\b('?[a-z])/) { $1.capitalize })
     if @ingredient.save
+      cabinet = Cabinet.create(user_id: current_user.id, ingredient_id: @ingredient.id, brand_id: 2)
       redirect_to "/users/#{current_user.id}"
     else
       render 'new'
