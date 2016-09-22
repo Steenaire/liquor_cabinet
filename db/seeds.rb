7abbdfeea3d1f63a91d1bfc40cb7a814f6f15f45
Steen Clarke <cmclarkey@gmail.com>
2016-09-22 13:16:43 -0700

    Install Faker gem, make a bunch of fake users and fake timeline drinks to test out how the heat map of drink locations looks

--- db/seeds.rb
+++ db/seeds.rb
@@ -159,3 +159,82 @@ TimelineDrink.create!([
   {user_id: 7, recipe_id: 9, comment: nil, image: nil},
   {user_id: 12, recipe_id: 11, comment: "Delicious, I love how the sweetness is balanced by the tartness is balanced by the complexity of this drink.", image: nil}
 ])
+
+10.times do |n|
+  user = User.new({username: Faker::Name.name.split[0], email: Faker::Internet.email, zip_code: Faker::Address.postcode})
+
+  user.password_digest = "password"
+
+  city = Geocoder.search(user.zip_code)
+
+  user.city = city[0].city
+  user.latitude = city[0].latitude
+  user.longitude = city[0].longitude
+
+  Timezone::Lookup.config(:google) do |c|
+    c.api_key = ENV['timezone_key']
+  end
+
+  user.timezone = Timezone.lookup(user.latitude, user.longitude)
+  user.save
+end
+
+user_ids = []
+recipe_ids = []
+users = User.all
+recipes = Recipe.all
+users.each do |user|
+  user_ids << user.id
+end
+recipes.each do |recipe|
+  recipe_ids << recipe.id
+end
+1000.times do |n|
+  timeline_drink=TimelineDrink.new(user_id: user_ids.sample, recipe_id: recipe_ids.sample)
+  timeline_drink.save
+end
+
+
+sf_zips = [94102, 94103, 94104, 94105, 94107, 94108, 94109, 94110, 94111, 94112, 94114, 94115, 94116, 94117, 94118, 94121, 94122, 94123, 94124, 94127, 94129, 94130, 94131, 94132, 94133, 94134, 94158]
+
+100.times do 
+  user = User.new({username: Faker::Name.name.split[0], email: Faker::Internet.email, zip_code: sf_zips.sample})
+
+  user.password_digest = "password"
+
+  city = Geocoder.search(user.zip_code)
+
+  user.city = city[0].city
+  user.latitude = city[0].latitude
+  user.longitude = city[0].longitude
+
+  Timezone::Lookup.config(:google) do |c|
+    c.api_key = ENV['timezone_key']
+  end
+
+  user.timezone = Timezone.lookup(user.latitude, user.longitude)
+  user.save
+end
+
+
+
+ny_zips = [10453, 10457, 10460,10458, 10467, 10468,10451, 10452, 10456,10454, 10455, 10459, 10474,10463, 10471,10466, 10469, 10470, 10475,10461, 10462,10464, 10465, 10472, 10473,11212, 11213, 11216, 11233, 11238,11209, 11214, 11228,11204, 11218, 11219, 11230,11234, 11236, 11239,11223, 11224, 11229, 11235,11201, 11205, 11215, 11217, 11231,11203, 11210, 11225, 11226,11207, 11208,11211, 11222,11220, 11232,11206, 11221, 11237,10026, 10027, 10030, 10037, 10039,10001, 10011, 10018, 10019, 10020, 10036,10029, 10035,10010, 10016, 10017, 10022,10012, 10013, 10014,10004, 10005, 10006, 10007, 10038, 10280,10002, 10003, 10009,10021, 10028, 10044, 10065, 10075, 10128,10023, 10024, 10025,10031, 10032, 10033, 10034, 10040,11361, 11362, 11363, 11364,11354, 11355, 11356, 11357, 11358, 11359, 11360,11365, 11366, 11367,11412, 11423, 11432, 11433, 11434, 11435, 11436,11101, 11102, 11103, 11104, 11105, 11106,11374, 11375, 11379, 11385,11691, 11692, 11693, 11694, 11695, 11697,11004, 11005, 11411, 11413, 11422, 11426, 11427, 11428, 11429,1414, 11415, 11416, 11417, 11418, 11419, 11420, 11421,11368, 11369, 11370, 11372, 11373, 11377, 11378,10302, 10303, 10310,10306, 10307, 10308, 10309, 10312,10301, 10304, 10305,10314]
+
+100.times do 
+  user = User.new({username: Faker::Name.name.split[0], email: Faker::Internet.email, zip_code: ny_zips.sample})
+
+  user.password_digest = "password"
+
+  city = Geocoder.search(user.zip_code)
+
+  user.city = city[0].city
+  user.latitude = city[0].latitude
+  user.longitude = city[0].longitude
+
+  Timezone::Lookup.config(:google) do |c|
+    c.api_key = ENV['timezone_key']
+  end
+
+  user.timezone = Timezone.lookup(user.latitude, user.longitude)
+  user.save
+end
